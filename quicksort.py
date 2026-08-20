e5c73cfaab1f2f4754ff41150798e6f5f2d97235
facundolgomez <facundolgomez87@gmail.com>
2026-08-20 10:49:51 -0300

    corrijo bug del algoritmo

--- quicksort.py
+++ quicksort.py
@@ -2,8 +2,8 @@ def quicksort(arr):
 	if len(arr) in (0,1):
 		return arr
 	pivot = arr[0]
-	less_than_pivot = [x for x in arr if x < pivot]
-	greater_than_pivot = [x for x in arr if x > pivot]
+	less_than_pivot = [x for x in arr[1:] if x <= pivot]
+	greater_than_pivot = [x for x in arr[1:] if x > pivot]
 	left_array = quicksort(less_than_pivot)
 	right_array = quicksort(greater_than_pivot)
 	return left_array + [pivot] + right_array
