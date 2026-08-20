853121a7165236a6cfc3d9cea283c98fcb9a2cd1
facundolgomez <facundolgomez87@gmail.com>
2026-08-19 22:33:00 -0300

    agrego algoritmo quicksort

---
+++ quicksort.py
@@ -0,0 +1,9 @@
+def quicksort(arr):
+	if len(arr) in (0,1):
+		return arr
+	pivot = arr[0]
+	less_than_pivot = [x for x in arr if x < pivot]
+	greater_than_pivot = [x for x in arr if x > pivot]
+	left_array = quicksort(less_than_pivot)
+	right_array = quicksort(greater_than_pivot)
+	return left_array + [pivot] + right_array
